b8be3f65cf5cb4791be84bbae5383a95a4758dc5
Parthak <parthirlplays@gmail.com>
2026-08-05 13:47:31 +0530

    Implemented React-Flask API communication with GET and POST

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -11,5 +11,15 @@ CORS(app)
 def home():
   return {"message": "Hello Parthak"}
 
+@app.route("/greet",methods=["POST"])
+
+def greet():
+
+  data = request.json
+  name=data.get("name")
+
+  return {
+    "message": f"Hello {name}"
+  }
 if __name__ == "__main__":
   app.run(debug=True)
